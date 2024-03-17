e2fb1c8389b379691d26a1bcdce4279144b49b4e
Vlad <evilspirt95@gmail.com>
2024-03-17 17:09:00 +0300

    - Добавил возможность взять айдишник юзера из токена для фильтрации

--- src/main/kotlin/com/mybar/bartender/controller/BarController.kt
+++ src/main/kotlin/com/mybar/bartender/controller/BarController.kt
@@ -3,22 +3,26 @@ package com.mybar.bartender.controller
 import com.mybar.bartender.dto.BarDto
 import com.mybar.bartender.service.BarService
 import com.mybar.bartender.model.Bar
+import com.mybar.bartender.service.AuthService
+
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/bars")
-class BarController(private val barService: BarService) {
+class BarController(private val barService: BarService, private val authService : AuthService) {
 
     @PostMapping
     fun createBar(@Valid @RequestBody barDto: BarDto): ResponseEntity<BarDto> {
+        // authService.getUserId();
         val createdBar = barService.createBar(barDto)
         return ResponseEntity.ok(createdBar.toDto())
     }
 
     @GetMapping
     fun getAllBars(): ResponseEntity<List<BarDto>> {
+        var id = authService.getUserId();
         val bars = barService.getAllBars().map { x -> x.toDto() }
         return ResponseEntity.ok(bars)
     }
